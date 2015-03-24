d35e178a038d452423634c47dc325548fede5e6c
Ben Glassman <bglassman@gmail.com>
2015-03-23 23:01:08 -0400

    Add delete confirmation.

--- src/components/ListDetail.react.js
+++ src/components/ListDetail.react.js
@@ -4,12 +4,24 @@ var ListActions = require('../actions/ListActions');
 
 var ListDetail = React.createClass({
 
+	getInitialState: function() {
+		return {
+			confirmed: false
+		}
+	},
+
   propTypes: {
     list: ReactPropTypes.object.isRequired
   },
 
 	on: {
 		delete: function(id) {
+			if (!this.state.confirmed) {
+				this.setState({
+					confirmed: true
+				});
+				return true;
+			}
 			ListActions.delete(id);
 		}
 	},
@@ -19,14 +31,16 @@ var ListDetail = React.createClass({
    */
   render: function() {
     return (
-		<div class="list">
+		<div className="list">
 			<h1>
 				{this.props.list.name}
-				<button class="list__delete" onClick={this.on.delete.bind(this, this.props.list.id)}>X</button>
+				<button className="list__delete" onClick={this.on.delete.bind(this, this.props.list.id)}>
+				{ this.state.confirmed ? 'Confirm' : 'X' }
+				</button>
 			</h1>
 		</div>
     );
-  },
+  }
 
 });
 
